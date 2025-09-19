28f8cbe3451c8837643a72b61689f166d815d00c
udhayan <88713829+udhayan4@users.noreply.github.com>
2025-09-19 11:09:43 +0530

    Time: 47 ms (93.88%) | Memory: 124.5 MB (86.70%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -0,0 +1,30 @@
+class Spreadsheet {
+public:
+    int rows;
+    unordered_map<string, int> cells;
+
+    Spreadsheet(int rows) {
+        this->rows = rows;
+    }
+    
+    void setCell(string cell, int value) {
+        cells[cell] = value;
+    }
+    
+    void resetCell(string cell) {
+        cells.erase(cell);
+    }
+    
+    int getValue(string formula) {
+        formula = formula.substr(1);
+        int plus = formula.find('+');
+        string a = formula.substr(0, plus);
+        string b = formula.substr(plus + 1);
+
+        auto val = [&](string s) {
+            return all_of(s.begin(), s.end(), ::isdigit) ? stoi(s) : cells[s];
+        };
+
+        return val(a) + val(b);
+    }
+};
