77b4eb430bb8618337841335d078ee19571eaa41
udhayan <88713829+udhayan4@users.noreply.github.com>
2025-09-25 17:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (91.62%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-
-    int minsum; 
-    int backtrack(vector<vector<int>>& tri , int n , int i , int j ,vector<vector<int>>& dp){
-        if(i > n-1 || j > i) return INT_MAX  ; 
-        if(i==n-1){
-            return tri[i][j];
-            // minsum = min(sum , minsum);
+    int minimumTotal(vector<vector<int>>& t) {
+        const int n=t.size();
+        for(int i=1; i<n; i++){
+            t[i][0]+=t[i-1][0];
+            t[i][i]+=t[i-1][i-1];
+            for(int j=1; j<i; j++){
+                t[i][j]+=min(t[i-1][j], t[i-1][j-1]);
+            }
         }
-        if(dp[i][j] != -1 ) return dp[i][j];
-        int lsum = backtrack(tri, n , i+1 , j, dp);
-        int rsum = backtrack(tri, n , i+1 , j+1, dp);
-        return dp[i][j] = tri[i][j] +  min(lsum, rsum);     
-        // sum = sum - tri[i][j];
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
-         minsum = INT_MAX; 
-        int rows = triangle.size();
-        vector<vector<int>> dp(rows, vector<int>(rows,-1)); 
-        if(rows == 1 ){
-            sort(triangle[0].begin(), triangle[0].end());
-            return triangle[0][0];
-        } 
-        // int sum = 0 ;
-        return backtrack(triangle , rows, 0 , 0 , dp);
-         
+        return *min_element(t[n-1].begin(), t[n-1].end());
     }
 };
